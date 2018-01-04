0e0f82b8d2aea659548655da4264caef03d283ac
Vladislav <slavayus@gmail.com>
2018-01-04 13:39:18 +0300

    rename main package

--- src/main/java/batman/Greeting.java
+++ src/main/java/batman/Greeting.java
@@ -1,4 +1,4 @@
-package hello;
+package batman;
 
 import database.repository.PointRepository;
 
--- src/main/java/batman/GreetingController.java
+++ src/main/java/batman/GreetingController.java
@@ -1,4 +1,4 @@
-package hello;
+package batman;
 
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
